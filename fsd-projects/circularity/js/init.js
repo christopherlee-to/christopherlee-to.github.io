323d8b04f7eac704c103e07d4a647b5a555fbab1
christopherlee-to <christopher.lee@inspire35.org>
2025-12-09 21:37:31 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -39,7 +39,9 @@ var circles = [];
         drawCircle();
         drawCircle();
         // TODO 7 : Use a loop to create multiple circles
-
+           for(var i = 0; i < 100; i++){
+           drawCircle();
+           }
 
 
 
@@ -54,20 +56,14 @@ var circles = [];
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-          physikz.updatePosition(circles[0]);
-          physikz.updatePosition(circles[1]);
-          physikz.updatePosition(circles[2]);
-          physikz.updatePosition(circles[3]);
-          physikz.updatePosition(circles[4]);
+         
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           game.checkCirclePosition(circles[0]);
-           game.checkCirclePosition(circles[1]);
-           game.checkCirclePosition(circles[2]);
-           game.checkCirclePosition(circles[3]);
-           game.checkCirclePosition(circles[4]);
+          
             // TODO 8 / TODO 9 : Iterate over the array
-           
-            
+           for(var i = 0; i < 100; i++){
+             physikz.updatePosition(circles[i]);
+              game.checkCirclePosition(circles[i]);
+           }        
         }
     
         /* 
@@ -81,10 +77,18 @@ var circles = [];
             if ( circle.x > canvas.width ) {
                 circle.x = 0;
             }
-            
+            if( circle.x < 0){
+                circle.x = canvas.width;
+            }
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if( circle.y > canvas.height){
+                circle.y = 0;
+            }
+            if( circle.y < 0){
+                circle.y = canvas.height;
+            }
 
+        
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
